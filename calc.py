c79194b3bde64cf50654070ef1bcbb428ca04efb
jeodev <jeodevacc@gmail.com>
2022-01-21 19:38:31 +0300

    Product function added

--- calc.py
+++ calc.py
@@ -3,5 +3,11 @@ def add(num_one, num_two):
     result = num_one + num_two
     return result
 
+def mul(num_one, num_two):
+    result = num_one * num_two
+    return result
+
 sum = add(3, 4)
+product = mul(3, 4)
 print(sum)
+print(product)
